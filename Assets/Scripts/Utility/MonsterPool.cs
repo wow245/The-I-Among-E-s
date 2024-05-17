2f24d522d27be455a96e892d49dc10085ffe0786
Sungjun Park <ddun4917@gmail.com>
2024-05-17 17:13:28 +0900

    Update: 코루틴사용

--- Assets/Scripts/Utility/MonsterPool.cs
+++ Assets/Scripts/Utility/MonsterPool.cs
@@ -10,13 +10,25 @@ public class MonsterPool : ObjectPool
     private void Start()
     {
         mapCollider = testMap.GetComponent<BoxCollider2D>();
-    }
+        StartCoroutine(SpawnMonster(9));
 
-    private void Update()
+        Debug.Log(GameManager.Instance.ObjectPool.PoolDictionary.Count);
+        Debug.Log(GameManager.Instance.ObjectPool.pools.Count);
+    }
+    
+    // TODO :: 실제 스폰처리는 다른 곳에서 함
+    // TODO :: 스폰 시 코루틴 사용       
+    IEnumerator SpawnMonster(int count)
     {
-        // TODO :: 실제 스폰처리는 다른 곳에서 함
-        GameObject monster = GameManager.Instance.ObjectPool.SpawnFromPool("Test");
-        monster.transform.position = ReturnRandomPos();
+        // TODO :: 반복 횟수 정하기
+        //         정해진 정수 or 몬스터 n마리 처치 시 종료?
+        for(int i=0;i<count;i++)
+        {           
+            GameObject monster = GameManager.Instance.ObjectPool.SpawnFromPool("Test");
+            monster.transform.position = ReturnRandomPos();
+
+            yield return new WaitForSeconds(1f);
+        }
     }
 
     // 몬스터를 랜덤한 위치에 생성하기 위해 랜덤 위치를 받아오는 함수
