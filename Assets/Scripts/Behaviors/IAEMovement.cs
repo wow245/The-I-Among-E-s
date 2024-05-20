8de9e031ea2ad438836ce486548aa08ca60f1670
siknyang <167048578+siknyang@users.noreply.github.com>
2024-05-20 20:19:51 +0900

    Feature: 넉백 구현

--- Assets/Scripts/Behaviors/IAEMovement.cs
+++ Assets/Scripts/Behaviors/IAEMovement.cs
@@ -8,6 +8,8 @@ public class IAEMovement : MonoBehaviour
     private Rigidbody2D movementRb;
     private Vector2 movementDir = Vector2.zero;
     private CharacterStatHandler characterStatHandler;
+    private Vector2 knockback = Vector2.zero;
+    private float knockbackDuration = 0.0f;
 
     private void Awake()
     {
@@ -29,11 +31,28 @@ public class IAEMovement : MonoBehaviour
     private void FixedUpdate()
     {
         ApplyMovement(movementDir);
+
+        if (knockbackDuration > 0.0f)
+        {
+            knockbackDuration -= Time.fixedDeltaTime;
+        }
+    }
+
+    public void ApplyKnockBack(Transform other, float power, float duration)
+    {
+        knockbackDuration = duration;
+        knockback = -(other.position - transform.position).normalized * power;  // 힘이 더해지며 뒤로 밀림
     }
 
     private void ApplyMovement(Vector2 direction)
     {
         direction *= characterStatHandler.CurrentStat.speed;
+
+        if (knockbackDuration > 0.0f)
+        {
+            direction += knockback;
+        }
+
         movementRb.velocity = direction;
     }
 }
