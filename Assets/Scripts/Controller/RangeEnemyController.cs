dbf28ceb8c48d17b1dab946dc87c4bf6aa52d2b9
siknyang <167048578+siknyang@users.noreply.github.com>
2024-05-20 14:42:48 +0900

    Feature: 범위 설정 및 기능 일부 구현

--- Assets/Scripts/Controller/RangeEnemyController.cs
+++ Assets/Scripts/Controller/RangeEnemyController.cs
@@ -1,4 +1,23 @@
-﻿public class RangeEnemyController : EnemyController
-{
+﻿using System;
+using UnityEngine;
 
+public class RangeEnemyController : EnemyController
+{
+    [SerializeField][Range(0f, 100f)] private float followRange = 15f;
+    [SerializeField][Range(0f, 100f)] private float shootRange = 10f;
+
+    private int layerMaskTarget;
+
+    protected override void Start()
+    {
+        base.Start();
+        // TODO :: CharacterStat.cs 추가
+        // layerMaskTarget = stats.CurrentStat.attackSO.target;
+    }
+
+    protected override void FixedUpdate()
+    {
+        base.FixedUpdate();
+        float distanceToTarget = DistanceToTarget();
+    }
 }
